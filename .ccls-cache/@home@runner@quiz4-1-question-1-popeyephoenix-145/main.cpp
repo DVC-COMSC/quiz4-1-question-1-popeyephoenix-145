050d03892b5110aa38f85e01203f91f08ba662cd
24David999 <112590764+popeyephoenix-145@users.noreply.github.com>
2022-10-12 22:52:36 +0000

    code

--- .ccls-cache/@home@runner@quiz4-1-question-1-popeyephoenix-145/main.cpp
+++ .ccls-cache/@home@runner@quiz4-1-question-1-popeyephoenix-145/main.cpp
@@ -1,24 +1,7 @@
 // Complete this program
-
 #include <iostream>
-using namespace std;
-
-int main()
-{
-	int choice;
-
-	double area;
-
-	cout << "Geometry Calculator\n";
-	cout << "\t1. Calculate the Area of Circle\n";
-	cout << "\t2. Calculate the Area of Rectangle\n";
-	cout << "\t3. Calculate the Area of Triangle\n";
-	cout << "\t4. Quit\n\n";
-	cout << "Enter your choice (1-4): ";
- #include <iostream>
-#include <iomanip>
 #include <cmath>
-
+#include <iomanip>
 using namespace std;
 int main() {
   int num;  
